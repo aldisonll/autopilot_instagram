065da633abe802af61eb57179e8955d35f77ebb5
aldisonll <34491203+aldisonll@users.noreply.github.com>
2021-02-16 21:42:40 +0100

    Update photo_scrapping.py

--- photo_scrapping.py
+++ photo_scrapping.py
@@ -5,6 +5,7 @@ toaster = ToastNotifier()
 headers = {'authority': 'www.instagram.com','cache-control': 'max-age=0','sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"','sec-ch-ua-mobile': '?0','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-site': 'none','sec-fetch-mode': 'navigate','sec-fetch-user': '?1','sec-fetch-dest': 'document','accept-language': 'en-US,en;q=0.9',}
 recent_value = ""
 
+print("Go to instagram and start copy links! Have fun!")
 while True:
     tmp_value = pyperclip.paste()
     if tmp_value != recent_value:
@@ -17,8 +18,8 @@ while True:
                 response = requests.get(url=json.loads(postApi.text)["graphql"]["shortcode_media"]["display_url"]) # get image link source
                 with open("photos/" + str(recent_value[29:39]) + ".jpg", "wb") as f: # downloading image to /photos folder
                     f.write(response.content)
-                    toaster.show_toast("Instagram AutoPilot 1.0", "Image Downloaded: " + str("https://www.instagram.com/" + recent_value[29:39]), icon_path="logo.ico",)
+                    toaster.show_toast("Instagram AutoPilot 1.0", "Image Downloaded: " + str("https://www.instagram.com/" + recent_value[29:39]), icon_path="logo.ico",) # send the notification (windows user)
                     print("Image Downloaded Successfuly")
            except:
-               print("Somthing's Wrong")         
+               print("Something's Wrong")         
                 
