f5439d00bc1c872403dba5193dd9b4ec6b72dfd0
aldisonll <34491203+aldisonll@users.noreply.github.com>
2021-02-16 21:44:53 +0100

    Update auto_upload.py

--- auto_upload.py
+++ auto_upload.py
@@ -1,6 +1,12 @@
 from instabot import Bot
 import os, time
 
+# settings 
+caption = '''Here is the caption
+#programming
+'''
+interval = 1 # post one time in 1 hour
+
 arr = os.listdir('./photos')
 
 bot = Bot() 
@@ -10,6 +16,6 @@ print(f'{len(arr)} Images Are Founded')
 for file in arr:
     if file[-4:] == ".jpg": # check if file name is .jpg
         img_path = 'photos/' + str(file) # get path of the picture
-        print(img_path)
-        bot.upload_photo(img_path, caption="#programming") # write a caption & upload
-        time.sleep(60*60*4) # wait 4 hour
+        print(img_path) 
+        bot.upload_photo(img_path, caption=caption) # write a caption & upload
+        time.sleep(60*60*interval) # time interval
